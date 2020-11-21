6dde05dc15e44b4bd4f8670576d4e321bdac7977
rifaiMuhamad7 <66523319+rifaiMuhamad7@users.noreply.github.com>
2020-11-21 12:39:07 +0700

    Update 01-resolusiProgramming.c

--- 01-resolusiProgramming.c
+++ 01-resolusiProgramming.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    printf("Pelajari text salah satu text editor, dan yang saya pilih adalah vim");
+    printf("Pelajari salah satu text editor, dan yang saya pilih adalah vim");
     printf("Pelajari Dasar Algoritma");
     printf("Pilih salah satu bahasa pemograman");
     printf("Konsisten untuk ngoding 1 jam dalam sehari");
