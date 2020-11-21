ab7829ad875f8b86e239a7d24ce62dfe77c6fe0a
rifaiMuhamad7 <66523319+rifaiMuhamad7@users.noreply.github.com>
2020-11-21 12:37:34 +0700

    Membuat_ProgramSederhana.c

---
+++ 01-resolusiProgramming.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("Pelajari text salah satu text editor, dan yang saya pilih adalah vim");
+    printf("Pelajari Dasar Algoritma");
+    printf("Pilih salah satu bahasa pemograman");
+    printf("Konsisten untuk ngoding 1 jam dalam sehari");
+    return 0;
+}
