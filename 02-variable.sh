cea55b0e510fd90d3c7d2e47eb4938002c45cbda
rifaiMuhamad7 <66523319+rifaiMuhamad7@users.noreply.github.com>
2020-12-26 16:13:26 +0700

    Add files via upload

---
+++ 02-variable.sh
@@ -0,0 +1,15 @@
+#! /usr/bin/bash
+
+#variable system
+echo $BASH
+echo $HOME
+echo $PWD
+echo $BASH_VERSION
+
+#user define variable
+firstname=Muhamad
+lastname=rifai
+old=32
+echo "The Full name is" $firstname $lastname
+echo "My age is" $old "years"
+
---
+++ 03-read.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+
+echo "enter your Name:"
+read yourname
+echo "your name is $yourname" 
