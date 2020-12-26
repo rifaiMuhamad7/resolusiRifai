21f45a87d80dcf520951d4fe4b445cfe252632e6
rifaiMuhamad7 <66523319+rifaiMuhamad7@users.noreply.github.com>
2020-12-26 16:44:39 +0700

    Update 03-read.sh

--- 03-read.sh
+++ 03-read.sh
@@ -1,5 +1,11 @@
 #! /usr/bin/bash
 
-echo "enter your Name:"
-read yourname
-echo "your name is $yourname" 
+echo "enter your operating system:"
+read operatingsystem
+echo "your operating system is $operatingsystem" 
+
+read -p 'Enter your username:' username
+read -sp 'Enter your password:' xxx
+echo
+echo "Username is: $username"
+echo "Your password is: $xxx"
